d69e37febb704b9f3a3271588602b6347659ab84
Carl Gudmundsson <komoss@gmail.com>
2022-08-03 13:30:01 +0200

    Implement recursive fibonacci

--- fibonacci.rb
+++ fibonacci.rb
@@ -4,12 +4,15 @@ def fib(num)
 end
 
 def fib_rec(num)
-  case num
-  when num == 1
-    0
-  when num == 2 
-    1
+  if num == 1
+    [0]
+  elsif num == 2
+    [0, 1]
   else
-    
+    prev = fib_rec(num - 1)
+    curr = prev[-1] + prev[-2]
+    prev.append(curr)
   end
 end
+
+p fib_rec(8)
